747180df90b1ec51b2f11d06727f04a80ae2475e
Thomas Koehn <thomas.koehn@mytum.de>
2012-06-12 12:42:19 -0700

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
 
-  before_save { |user| user.email = user.email.downcase }
+  before_save { self.email.downcase! }
 
   # VALIDATIONS
   validates :name, presence: true, length: { maximum: 50 }
@@ -21,6 +21,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive:false }
-  validates :password, presence:true, length: { minimum:6 }
+  validates :password, length: { minimum:6 }, presence:true
   validates :password_confirmation, presence:true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,10 +13,10 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(name:"Example User", email:"email@example.com",
-                      password:"foobar", password_confirmation:"foobar") }
-
   subject { @user }
+
+  before { @user = User.new(name:"Example User", email:"email@example.com",
+                                password:"foobar", password_confirmation:"foobar") }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
@@ -112,6 +112,16 @@ describe User do
 
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
+    end
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
     end
   end
 
