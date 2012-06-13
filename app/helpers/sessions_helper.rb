c670d778cf7ba11c0826401edadf0d3dc7dde130
Thomas Koehn <thomas.koehn@mytum.de>
2012-06-13 01:20:37 -0700

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,4 +21,17 @@ module SessionsHelper
   def signed_in?
     !self.current_user.nil?
   end
+
+  def current_user?(user)
+    self.current_user == user
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.fullpath
+  end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -6,6 +6,15 @@ def valid_signin(user)
   click_button "Sign in"
 end
 
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
 RSpec::Matchers.define :have_error_message do |message|
   match do |page|
     page.should have_selector('div.alert.alert-error', text: message)
@@ -15,5 +24,11 @@ end
 RSpec::Matchers.define :have_title_selector do |message|
   match do |page|
     page.should have_selector('title', text: message)
+  end
+end
+
+RSpec::Matchers.define :have_h1_selector do |message|
+  match do |page|
+    page.should have_selector('h1', text: message)
   end
 end
